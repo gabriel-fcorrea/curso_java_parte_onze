721137fdeeaa8665e5764099c4bc2078f69d302a
gabriel-fcorrea <gabriel.correa.dev@gmail.com>
2025-05-09 10:33:45 -0300

    novo exemplo de Map, usando equals e hashcode

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -7,6 +7,10 @@ public class Product implements Comparable<Product> {
 	private String name;
 	private Double price;
 
+	public Product(String name) {
+		this.name = name;
+	}
+
 	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
